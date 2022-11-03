e67ce6f7e215b464653338cfe5f50c0504e78f2e
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-11-02 17:52:31 -0700

    finished the index model file

--- models/index.js
+++ models/index.js
@@ -5,16 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+	foreignKey: 'category_id',
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+	foreignKey: 'category_id',
+	onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+	through: 'ProductTag',
+	foreignKey: 'tag_id',
+	otherKey: '',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+	through: 'ProductTag',
+	foreignKey: 'product_id',
+	otherKey: 'id',
+});
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+	Product,
+	Category,
+	Tag,
+	ProductTag,
 };
