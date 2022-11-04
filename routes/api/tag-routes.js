f79552dd6b075207569919733b575dfb0b4f9f18
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-11-03 22:46:56 -0700

    finished project

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -27,46 +27,76 @@ router.get('/:id', async (req, res) => {
 	}
 });
 
-router.post('/',async (req, res) => {
-  // Tag.create(req.body)
-  // .then((tag) => {
-  //   if (req.body.productIds.length) {
-  //     const productTagIdArr = req.body.productIds.map((product_id) => {
-  //       return {
-  //         tag_id: tag.id,
-  //         product_id,
-  //       };
-  //     });
-  //     return ProductTag.bulkCreate(productTagIdArr);
-  //   }
-  //   // if no product tags, just respond
-  //   res.json({ message: "Created successfully"});
-  // })
-  // .then((productTagIds) => res.json({ message: "Created successfully"}))
-  // .catch((err) => {
-  //   console.log(err);
-  //   res.json(err);
-  // });
+router.post('/', (req, res) => {
+  Tag.create(req.body)
+  .then((tag) => {
+    if (req.body.productIds.length) {
+      const productTagIdArr = req.body.productIds.map((product_id) => {
+        return {
+          tag_id: tag.id,
+          product_id,
+        };
+      });
+      return ProductTag.bulkCreate(productTagIdArr);
+    }
+    // if no product tags, just respond
+    res.json({ message: "Created successfully"});
+  })
+  .then((productTagIds) => res.json({ message: "Created successfully"}))
+  .catch((err) => {
+    console.log(err);
+    res.json(err);
+  });
 
-	try {
-		const newTag = await Tag.create(req.body);
-		if (req.body.productIds.length) {
-			const productTagIdArr = req.body.productIds.map((product_id) => {
-				return {
-					tag_id: newTag.id,
-					product_id,
-				};
-			});
-			return ProductTag.bulkCreate(productTagIdArr);
-		}
-		res.json({ message: 'Created successfully' });
-	} catch (err) {
-		res.json(err);
-	}
+	// try {
+	// 	const newTag = await Tag.create(req.body);
+	// 	if (req.body.productIds.length) {
+	// 		const productTagIdArr = req.body.productIds.map((product_id) => {
+	// 			return {
+	// 				tag_id: newTag.id,
+	// 				product_id,
+	// 			};
+	// 		});
+	// 		return ProductTag.bulkCreate(productTagIdArr);
+	// 	}
+	// 	res.json({ message: 'Created successfully' });
+	// } catch (err) {
+	// 	res.json(err);
+	// }
 });
 
-router.put('/:id', (req, res) => {
-	// update a tag's name by its `id` value
+router.put('/:id', async(req, res) => {
+	try {
+    await Tag.update(req.body, {
+     where: {
+       id: req.params.id,
+     },
+   });
+   const prodTags = await ProductTag.findAll({
+     where: { tag_id: req.params.id },
+   });
+   if (prodTags) {
+     const productTagIds = prodTags.map(({ product_id }) => product_id);
+     const newProductTags = req.body.productIds
+       .filter((product_id) => !productTagIds.includes(product_id))
+       .map((product_id) => {
+         return {
+           tag_id: req.params.id,
+           product_id,
+         };
+       });
+     // figure out which ones to remove
+     const tagsToRemove = prodTags
+       .filter(({ product_id }) => !req.body.productIds.includes(product_id))
+       .map(({ id }) => id);
+
+     await ProductTag.destroy({ where: { id: tagsToRemove } });
+     await ProductTag.bulkCreate(newProductTags);
+     res.json({ message: "Updated successfully"});
+   }
+ } catch (err) {
+   res.json(err);
+ }
 });
 
 // delete on tag by its `id` value
